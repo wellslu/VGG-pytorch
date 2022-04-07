eb52b975d3b1a1f236c6e111106ec1884c19ca64
wellslu <34208509+wellslu@users.noreply.github.com>
2022-04-07 14:02:42 +0800

    Add files via upload

--- src/models/vgg.py
+++ src/models/vgg.py
@@ -1,10 +1,10 @@
 import mlconfig
 from torch import nn
 
-vgg_selector = {11:((64, 1), (128, 1), (256, 2), (512, 2), (512, 2)),
-               13:((64, 2), (128, 2), (256, 2), (512, 2), (512, 2)),
-               16:((64, 2), (128, 2), (256, 3), (512, 3), (512, 3)),
-               19:((64, 2), (128, 2), (256, 4), (512, 4), (512, 4))}
+vgg_selector = {11:((3, 64, 1), (64, 128, 1), (128, 256, 2), (256, 512, 2), (512, 512, 2)),
+               13:((3, 64, 2), (64, 128, 2), (128, 256, 2), (256, 512, 2), (512, 512, 2)),
+               16:((3, 64, 2), (64, 128, 2), (128, 256, 3), (256, 512, 3), (512, 512, 3)),
+               19:((3, 64, 2), (64, 128, 2), (128, 256, 4), (256, 512, 4), (512, 512, 4))}
 
 @mlconfig.register
 class VGG(nn.Module):
@@ -13,8 +13,7 @@ class VGG(nn.Module):
         super(VGG, self).__init__()
         arch = vgg_selector[arch]
         layers = []
-        self.in_channels = 3
-        for i, a in enumerate(arch):
+        for a in arch:
             layers+=self.ConvBNReLU_Block(a)
         self.features = nn.Sequential(*layers)
 
@@ -26,8 +25,20 @@ class VGG(nn.Module):
             nn.Dropout(0.5),
             nn.ReLU(inplace=True),
             nn.Linear(4096, 2),
-            nn.LogSoftmax(dim=1)
+            nn.Softmax(dim=1)
         )
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
 
     def forward(self, x):
         x = self.features(x)
@@ -38,11 +49,11 @@ class VGG(nn.Module):
     
     def ConvBNReLU_Block(self, arch):
         layer = []
-        out_channels, conv_num = arch
+        in_channels, out_channels, conv_num = arch
         for i in range(conv_num):
-            layer.append(nn.Conv2d(self.in_channels, out_channels, (3, 3), padding=1))
+            layer.append(nn.Conv2d(in_channels, out_channels, (3, 3), padding=1))
             # layer.append(nn.BatchNorm2d(out_channels))
             layer.append(nn.ReLU(inplace=True))
-            self.in_channels = out_channels
+            in_channels = out_channels
         layer.append(nn.MaxPool2d(2, 2))
         return layer
